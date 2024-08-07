ec5cbc9249a7e55b12aa6201e6b8b9f066073169
Giovanna Bataglini <gibataglini@gmail.com>
2024-08-07 12:07:45 +0100

    DB schema added, docker compose now creates the db

---
+++ schema/db_schema.sql
@@ -0,0 +1,59 @@
+CREATE TABLE user_details (
+    id SERIAL PRIMARY KEY NOT NULL, 
+    first_name VARCHAR (255) NOT NULL,  
+    last_name VARCHAR (255) NOT NULL, 
+    email VARCHAR (255) NOT NULL, 
+    date_of_birth DATE, 
+    created_at TIMESTAMP, 
+    modified_at TIMESTAMP
+); 
+
+
+CREATE TABLE destinations (
+    id SERIAL PRIMARY KEY NOT NULL, 
+    user_id INT NOT NULL REFERENCES user_details (id), 
+    city VARCHAR (255) NOT NULL, 
+    country VARCHAR (255) NOT NULL,
+    visited BOOLEAN, 
+    created_at TIMESTAMP, 
+    modified_at TIMESTAMP 
+); 
+
+CREATE TABLE journi_planner (
+    id SERIAL PRIMARY KEY NOT NULL, 
+    destination_id INT NOT NULL REFERENCES destinations (id),
+    user_id INT NOT NULL REFERENCES user_details (id), 
+    date_from TIMESTAMP, 
+    date_to TIMESTAMP, 
+    accommodation JSON, 
+    trip_type VARCHAR (255), 
+    created_at TIMESTAMP, 
+    modified_at TIMESTAMP
+); 
+
+
+CREATE TABLE journi_plans (
+    id SERIAL PRIMARY KEY NOT NULL, 
+    destination_id INT NOT NULL REFERENCES destinations (id),
+    user_id INT NOT NULL REFERENCES user_details (id), 
+    planner_id INT NOT NULL REFERENCES journi_planner (id), 
+    location_title VARCHAR (255) NOT NULL, 
+    location_address VARCHAR (255) NOT NULL, 
+    location_type VARCHAR (255) NOT NULL, 
+    trip_duration INT, 
+    planned_visit_day INT, 
+    created_at TIMESTAMP, 
+    modified_at TIMESTAMP
+);
+
+CREATE TABLE journal_entries (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INT NOT NULL REFERENCES user_details (id), 
+    destination_id INT NOT NULL REFERENCES destinations (id),
+    planner_id INT NOT NULL REFERENCES journi_planner (id), 
+    entry_text TEXT,
+    entry_location JSON, 
+    media TEXT,
+    created_at TIMESTAMP, 
+    modified_at TIMESTAMP
+); 
